5fbf9712f54e45736bbc3a23ef091835b782e358
madhr <magdahrebecka@gmail.com>
2020-02-06 00:39:39 +0100

    main class, get weather from api, create midi file

---
+++ main.py
@@ -0,0 +1,53 @@
+from mido import MidiFile
+from pathlib import Path
+import instruments
+import weather_api
+from arpeggiator import ArpPattern
+from chords import Chords
+from track_builder import TrackBuilder
+
+if __name__ == '__main__':
+
+	# get weather from api and parse it
+
+	API_KEY = 'test'
+	city = 'Wroclaw,pl'
+
+	api_handling = weather_api.WeatherApi()
+
+	json_weather_forecast = api_handling.get_json_weather_forecast_from_api(city, API_KEY)
+	weather_forecast = api_handling.parse_weather_forecast(json_weather_forecast)
+
+	print("Weather forecast successfully parsed")
+
+	# create and save midi file
+
+	outfile = MidiFile()
+	track_builder = TrackBuilder()
+	chords = Chords()
+
+	SONG_LENGTH = 100000
+	base_note = 60
+
+	outfile = track_builder.add_random_scale(
+		outfile=outfile,
+		program_value=instruments.Instruments.Celesta.value,
+		channel=0,
+		scale=chords.minor_seventh(base_note),
+		time_limit=SONG_LENGTH
+	)
+
+	outfile = track_builder.add_arpeggiator(
+		outfile=outfile,
+		program_value=instruments.Instruments.Piccolo.value,
+		channel=1,
+		pattern=ArpPattern.UP,
+		scale=chords.minor_triad(base_note),
+		time_limit=SONG_LENGTH
+	)
+
+	file_dir = 'midi_out'
+	file_name = 'song-for-'+city+'.mid'
+	Path(file_dir).mkdir(exist_ok=True)
+	outfile.save(file_dir+'/'+file_name)
+	print("file saved at "+file_dir+'/'+file_name)
