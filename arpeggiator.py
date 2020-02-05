f74fbd4d53dc60de049757b07fc5c6ee945e16e3
madhr <magdahrebecka@gmail.com>
2020-02-06 00:39:01 +0100

    add track builder and arpeggiator classes

---
+++ arpeggiator.py
@@ -0,0 +1,28 @@
+from enum import Enum, auto
+
+class Arpeggiator:
+
+    def create(self, pattern, list_of_notes):
+        if pattern == ArpPattern.UP:
+            return self.__up(list_of_notes)
+        elif pattern == ArpPattern.DOWN:
+            return self.__down(list_of_notes)
+        elif pattern == ArpPattern.UP_AND_DOWN:
+            return self.__up_and_down(list_of_notes)
+        else:
+            raise Exception("Invalid ArpPattern enum")
+
+    def __up(self, list_of_notes):
+        return list_of_notes
+
+    def __down(self, list_of_notes):
+        return list(reversed(list_of_notes))
+
+    def __up_and_down(self, list_of_notes):
+        return self.up(list_of_notes) + self.down(list_of_notes[1:-1])
+
+class ArpPattern(Enum):
+    UP = auto()
+    DOWN = auto()
+    UP_AND_DOWN = auto()
+
---
+++ track_builder.py
@@ -0,0 +1,42 @@
+import random
+from arpeggiator import Arpeggiator
+from mido import Message, MidiTrack
+
+
+class TrackBuilder:
+
+
+	def add_random_scale(self, outfile, program_value, channel, scale, time_limit):
+		track = MidiTrack()
+		outfile.tracks.append(track)
+		track.name = 'random_scale'
+		track.append(Message('program_change', channel=channel, program=program_value, time=0))
+		sum = 0
+		while sum < time_limit:
+			note = random.choice(scale)
+			time = random.randrange(120, 600, 60)
+			sum += time
+			track.append(Message('note_on', note=note, channel=channel, velocity=60, time=time))
+			track.append(Message('note_off', note=note, channel=channel, velocity=60, time=time))
+		return outfile
+
+	def add_arpeggiator(self, outfile, program_value, channel, pattern, scale, time_limit):
+		track1 = MidiTrack()
+		outfile.tracks.append(track1)
+		track1.name = "arp"
+		track1.append(Message('program_change', channel=channel, program=program_value, time=0))
+		arpeggiator = Arpeggiator()
+		arp_up_and_down = arpeggiator.create(pattern, scale)
+		count = 0
+		notes_size = len(arp_up_and_down)
+		sum = 0
+		while sum < time_limit:
+			if count == notes_size:
+				count = 0
+			note = arp_up_and_down[count]
+			count = count + 1
+			time = 150
+			sum += time
+			track1.append(Message('note_on', note=note, channel=channel, velocity=60, time=time))
+			track1.append(Message('note_off', note=note, channel=channel, velocity=60, time=time))
+		return outfile
