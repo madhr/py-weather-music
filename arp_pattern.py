610c757f64d40e0a789106b5f01614f0f8f43b8c
madhr <magdahrebecka@gmail.com>
2020-02-09 17:46:08 +0100

    add arp patterns

--- arp_pattern.py
+++ arp_pattern.py
@@ -2,5 +2,8 @@ from enum import Enum, auto
 
 class ArpPattern(Enum):
     UP = auto()
+    UP_2_OCTAVES = auto()
     DOWN = auto()
+    DOWN_2_OCTAVES = auto()
     UP_AND_DOWN = auto()
+    UP_AND_DOWN_2_OCTAVES = auto()
