ddfb6f0246afe1290f2feb5696654e33cf2770b9
madhr <magdahrebecka@gmail.com>
2020-02-09 21:36:17 +0100

    add more transposition methods

--- transposer.py
+++ transposer.py
@@ -1,5 +1,7 @@
 class Transposer:
 
+    OCTAVE = 12
+
     def transpose(self, note: int, value: int) -> int:
         if note is None:
             return None
@@ -14,20 +16,40 @@ class Transposer:
     def octave_up(self, note: int) -> int:
         if note is None:
             return None
-        return self.transpose(note, 12)
+        return self.transpose(note, self.OCTAVE)
 
     def octave_up_list(self, notes: list) -> list:
         if notes is None:
             return None
-        return self.transpose_list(notes, 12)
+        return self.transpose_list(notes, self.OCTAVE)
 
     def octave_down(self, note: int) -> int:
         if note is None:
             return None
-        return self.transpose(note, -12)
+        return self.transpose(note, self.OCTAVE * (-1))
 
     def octave_down_list(self, notes: list) -> list:
         if notes is None:
             return None
-        return self.transpose_list(notes, -12)
+        return self.transpose_list(notes, self.OCTAVE * (-1))
+
+    def two_octaves_up(self, note: int) -> int:
+        if note is None:
+            return None
+        return self.transpose(note, self.OCTAVE * 2)
+
+    def two_octaves_up_list(self, notes: list) -> list:
+        if notes is None:
+            return None
+        return self.transpose_list(notes, self.OCTAVE * 2)
+
+    def two_octaves_down(self, note: int) -> int:
+        if note is None:
+            return None
+        return self.transpose(note, self.OCTAVE * (-2))
+
+    def two_octaves_down_list(self, notes: list) -> list:
+        if notes is None:
+            return None
+        return self.transpose_list(notes, self.OCTAVE * (-2))
 
