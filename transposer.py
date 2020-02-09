1952c16e52c7a7a2bbb7b13cba243432a54e592a
madhr <magdahrebecka@gmail.com>
2020-02-09 17:39:07 +0100

    add none checks to transposer

--- transposer.py
+++ transposer.py
@@ -1,21 +1,33 @@
 class Transposer:
 
-    def transpose(self, note: int, value: int):
+    def transpose(self, note: int, value: int) -> int:
+        if note is None:
+            return None
         return note + value
 
-    def transpose_list(self, notes: list, value: int):
+    def transpose_list(self, notes: list, value: int) -> list:
+        if notes is None:
+            return None
         transposed = [note+value for note in notes]
         return transposed
 
-    def octave_up(self, note):
+    def octave_up(self, note: int) -> int:
+        if note is None:
+            return None
         return self.transpose(note, 12)
 
-    def octave_up_list(self, notes: list):
+    def octave_up_list(self, notes: list) -> list:
+        if notes is None:
+            return None
         return self.transpose_list(notes, 12)
 
-    def octave_down(self, note):
+    def octave_down(self, note: int) -> int:
+        if note is None:
+            return None
         return self.transpose(note, -12)
 
-    def octave_down_list(self, notes: list):
+    def octave_down_list(self, notes: list) -> list:
+        if notes is None:
+            return None
         return self.transpose_list(notes, -12)
 
