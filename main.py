5e5ea5553f5257b0e21707a142cf17a5b4fb58f8
Magda <magdahrebecka@gmail.com>
2020-02-11 17:50:36 +0000

    fix spacing in main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ API_KEY = '7f1f66212a27b1173d1c96f3f644b3a5'
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Pass city name and two digit country code')
-	parser.add_argument('city', metavar='city', type=str, nargs=1,
-	                    help='city name')
-	parser.add_argument('country', metavar='country', type=str, nargs=1,
-	                    help='2 digit country code ISO 3166-1 compliant')
+	parser.add_argument('city', metavar='city', type=str, nargs=1, help='city name')
+	parser.add_argument('country', metavar='country', type=str, nargs=1, help='2 digit country code ISO 3166-1 compliant')
 
 	args = parser.parse_args()
 
