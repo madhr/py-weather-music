b1162f783453a8d2f75345f28016bf2c2fe68f52
madhr <magdahrebecka@gmail.com>
2020-02-11 22:08:40 +0100

    refactor rain appender and sequence

--- appenders/rain_appender.py
+++ appenders/rain_appender.py
@@ -1,27 +1,19 @@
 from mido import MidiFile
 
 from appenders.appender_interface import AppenderInterface
-from converters import Converter
 from melody_builder import MelodyBuilder
 from sequences.rain_sequence import RainSequence
-from transposer import Transposer
 from util.instruments import Instruments
 
 
 class RainAppender(AppenderInterface):
 
-	converter = Converter()
-	transposer = Transposer()
-
 	def append(self, melody_builder: MelodyBuilder, rain_sequence: RainSequence) -> MidiFile:
-
-		rain_scale = self.converter.rain_to_scale_size(rain_sequence.get_rain(), rain_sequence.get_music_scale())
-		rain_scale = self.transposer.octave_up_list(rain_scale)
 
 		melody_builder.outfile = melody_builder.random(
 			program_value=Instruments.Celesta,
 			channel=2,
-			scale=rain_scale,
+			scale=rain_sequence.get_rain_scale(),
 			track=rain_sequence.get_track(),
 			time_factor=15
 		)
