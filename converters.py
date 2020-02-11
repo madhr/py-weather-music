fc501e1fd2700d6bf0dd77c1b52917d875d86040
madhr <magdahrebecka@gmail.com>
2020-02-11 22:47:00 +0100

    extract constants for coverters

--- converters.py
+++ converters.py
@@ -6,6 +6,16 @@ from transposer import Transposer
 
 class Converter:
 
+	HUMIDITY_TO_VOLUME = 1.27
+	WIND_TO_NOISE = 5
+	CLOUDS_TO_CHORD_LENGTH = 12
+	TEMPERATURE_TO_BASE_NOTE = 0.238
+	TRANSPOSE_BASE_NOTE = -6
+	MAX_NOISE_VALUE = 127
+	EDGE_WIND_VALUE = 25
+	AMPLITUDE_ADD = 1
+	AMPLITUDE_MULTIPLY = 4
+
 	def list_of_notes_to_length(self, list_of_notes: list, length: int) -> list:
 		if len(list_of_notes) > length:
 			return list_of_notes[0:length]
@@ -16,12 +26,13 @@ class Converter:
 
 	def amplitude_to_arp_notes(self, amplitude: int, list_of_notes: list) -> list:
 		arpeggiator = Arpeggiator()
-		normalized_amplitude = int(amplitude + 1) * 4
+		normalized_amplitude = int(amplitude + self.AMPLITUDE_ADD) * self.AMPLITUDE_MULTIPLY
 		amplitude_based_notes = self.list_of_notes_to_length(list_of_notes, normalized_amplitude)
 		return arpeggiator.create(ArpPattern.UP_AND_DOWN, amplitude_based_notes)
 
 	def temperature_to_base_note(self, feels_like: float) -> int:
-		return int(feels_like / 4.2) - 6
+		transposer = Transposer()
+		return transposer.transpose(int(feels_like * self.TEMPERATURE_TO_BASE_NOTE), self.TRANSPOSE_BASE_NOTE)
 
 	def rain_to_scale_size(self, rain: float, scale: list) -> list:
 		if len(scale) < 1:
@@ -38,10 +49,10 @@ class Converter:
 		return scale[0:last_item]
 
 	def clouds_to_chord_length(self, clouds: int) -> int:
-		return int(clouds * 12)
+		return int(clouds * self.CLOUDS_TO_CHORD_LENGTH)
 
 	def humidity_to_volume(self, humidity: int):
-		return int(humidity * 1.27)
+		return int(humidity * self.HUMIDITY_TO_VOLUME)
 
 	def wind_to_noise_level(self, wind: float) -> int:
-		return int(wind*5) if wind < 25 else 127
+		return int(wind*self.WIND_TO_NOISE) if wind < self.EDGE_WIND_VALUE else self.MAX_NOISE_VALUE
