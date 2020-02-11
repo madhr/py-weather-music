f2b5adcbddf35c79ae2616415ecfd45a169c25b8
madhr <magdahrebecka@gmail.com>
2020-02-11 20:44:59 +0100

    rename music_scale class, add return types

--- util/music_scale.py
+++ util/music_scale.py
@@ -1,13 +1,13 @@
-class MusicScales:
+class MusicScale:
 
-	def pentatonic(self, note):
+	def pentatonic(self, note) -> list:
 		return [note, note + 3, note + 5, note + 7, note + 10]
 
-	def melodic_minor(self, note):
+	def melodic_minor(self, note) -> list:
 		return [note, note + 2, note + 3, note + 5, note + 7, note + 8, note + 11]
 
-	def minor(self, note):
+	def minor(self, note) -> list:
 		return [note, note + 2, note + 3, note + 5, note + 7, note + 8, note + 10]
 
-	def major(self, note):
+	def major(self, note) -> list:
 		return [note, note + 2, note + 4, note + 5, note + 7, note + 9, note + 11]
