08e20caa4a2127f518543de269fa3e6988b32f65
madhr <magdahrebecka@gmail.com>
2020-02-11 20:47:07 +0100

    add music_scale field to sequence class

--- sequences/sequence.py
+++ sequences/sequence.py
@@ -1,12 +1,15 @@
 from mido import MidiTrack
 
+from util.music_scale import MusicScale
+
 
 class Sequence:
 
-	def __init__(self, length: int, base_note: int, track: MidiTrack):
+	def __init__(self, length: int, base_note: int, track: MidiTrack, music_scale=None):
 		self.__length = length
 		self.__base_note = base_note
 		self.__track = track
+		self.__music_scale = music_scale
 
 	def get_length(self):
 		return self.__length
@@ -17,6 +20,12 @@ class Sequence:
 	def get_track(self):
 		return self.__track
 
+	def get_music_scale(self):
+		return self.__music_scale
+
 	def set_base_note(self, base_note: int):
 		self.__base_note = base_note
+
+	def set_music_scale(self, music_scale: MusicScale):
+		self.__music_scale = music_scale
 
