9f3b32d3549fe4855aaa3824bd886ddc125a0fa5
madhr <magdahrebecka@gmail.com>
2020-02-11 22:04:43 +0100

    refactor clouds appender and sequence

--- appenders/clouds_appender.py
+++ appenders/clouds_appender.py
@@ -1,30 +1,21 @@
 from mido import MidiFile
 
 from appenders.appender_interface import AppenderInterface
-from converters import Converter
 from melody_builder import MelodyBuilder
 from sequences.clouds_sequence import CloudsSequence
-from transposer import Transposer
-from util.chords import Chords
 from util.instruments import Instruments
 
 
 class CloudsAppender(AppenderInterface):
 
-	chords = Chords()
-	converter = Converter()
-	transposer = Transposer()
-
 	def append(self, melody_builder: MelodyBuilder, clouds_sequence: CloudsSequence) -> MidiFile:
-
-		length = self.converter.clouds_to_chord_length(clouds_sequence.get_clouds())
 
 		melody_builder.outfile = melody_builder.chord(
 			program_value=Instruments.TremoloStrings,
 			channel=3,
-			chord=self.transposer.octave_down_list(self.chords.minor_seventh(clouds_sequence.get_base_note())),
+			chord=clouds_sequence.get_chord(),
 			track=clouds_sequence.get_track(),
-			time_factor=length
+			time_factor=clouds_sequence.get_length()
 		)
 
 		return melody_builder.outfile
--- sequences/clouds_sequence.py
+++ sequences/clouds_sequence.py
@@ -1,14 +1,23 @@
 from mido import MidiTrack
 
+from converters import Converter
 from sequences.sequence import Sequence
+from transposer import Transposer
+from util.chords import Chords
 
 
 class CloudsSequence(Sequence):
 
-	def __init__(self, clouds: float, length: int, base_note: int, track: MidiTrack, music_scale=None):
+	chords = Chords()
+	converter = Converter()
+	transposer = Transposer()
+
+	def __init__(self, clouds: int, length: int, base_note: int, track: MidiTrack, music_scale=None):
 		super().__init__(length, base_note, music_scale)
 		self.__clouds = clouds
 		self.__track = track
+		self.__length = self.converter.clouds_to_chord_length(self.get_clouds())
+		self.__chord = self.transposer.octave_down_list(self.chords.minor_seventh(self.get_base_note()))
 
 	def get_clouds(self):
 		return self.__clouds
@@ -18,4 +27,10 @@ class CloudsSequence(Sequence):
 
 	def get_track(self):
 		return self.__track
+
+	def get_length(self):
+		return self.__length
+
+	def get_chord(self):
+		return self.__chord
 
