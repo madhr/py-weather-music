8c8c6ccbb5408624c768c6429fa3807698b68fd4
Magda <magdahrebecka@gmail.com>
2020-02-13 16:01:07 +0000

    update tests for melody builder

--- tests/test_melody_builder.py
+++ tests/test_melody_builder.py
@@ -2,6 +2,9 @@ from unittest import TestCase
 
 from mido import MidiFile, MidiTrack
 
+from app.util.arp_pattern import ArpPattern
+from app.util.chords import Chords
+from app.util.instruments import Instruments
 from app.util.melody_builder import MelodyBuilder
 
 
@@ -10,22 +13,51 @@ class TestMelodyBuilder(TestCase):
 
 
     def test_random(self):
-        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=120)
+        time_limit = 120
         track = MidiTrack()
-        channel = 1
-        scale = [5, 7]
-        random = melody_builder.random(channel, scale, track)
-        self.fail()
+        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=time_limit)
+        melody_builder.random(channel=1, scale=[5, 7], track=track)
+        sum = 0
+        for mes in track:
+            sum += mes.time
+        self.assertEqual(sum, time_limit * 2)
 
 
     def test_arpeggiator(self):
-        self.fail()
+        time_limit = 120
+        track = MidiTrack()
+        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=time_limit)
+        melody_builder.arpeggiator(channel=1, pattern=ArpPattern.UP, track=track, time_factor=time_limit, scale=[5, 7])
+        sum = 0
+        for mes in track:
+            sum += mes.time
+        self.assertEqual(sum, time_limit * 2)
 
     def test_chord(self):
-        self.fail()
+        time_limit = 120
+        track = MidiTrack()
+        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=time_limit)
+        chords = Chords()
+        melody_builder.chord(channel=1, chord=chords.major_seventh(note=60), track=track, time_factor=time_limit)
+        sum = 0
+        for mes in track:
+            sum += mes.time
+        self.assertEqual(sum, time_limit * 2)
 
     def test_dynamic(self):
-        self.fail()
+        time_limit = 120
+        track = MidiTrack()
+        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=time_limit)
+        melody_builder.dynamic(channel=1, note=60, track=track, velocity=60)
+        sum = 0
+        for mes in track:
+            sum += mes.time
+        self.assertEqual(sum, time_limit * 2)
 
     def test_set_instrument(self):
-        self.fail()
+        melody_builder = MelodyBuilder(outfile=MidiFile(), time_limit=120)
+        track = MidiTrack()
+        program_value = Instruments.Seashore
+        melody_builder.set_instrument(track=track, channel=1, program_value=program_value)
+
+        self.assertEqual(track[0].program, program_value)
